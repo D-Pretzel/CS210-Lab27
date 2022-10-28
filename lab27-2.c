fca0ba52cbe124b9c0ec028dcf0d9a4b27c5d333
David Petzold <petzoldavid02@gmail.com>
2022-10-28 11:47:14 -0600

    Problem with reading from file?

--- lab27-2.c
+++ lab27-2.c
@@ -7,9 +7,34 @@
 
 #include <stdio.h>
 
+//Writes the multiplication table for a given integer to a text file
 int main(void) {
+    // Ask for an integer
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
-    
+    // Ask for an ending int
+    int end;
+    printf("Enter an ending integer: ");
+    scanf("%d", &end);
+
+    // Ask for a file name
+    char fileName[20];
+    printf("Enter a file name: ");
+    scanf("%s", fileName);
+
+    // Open the file
+    FILE *file = fopen(fileName, "w");
+
+    // Write the multiplication table to the file starting with 0
+    for (int i = 0; i <= end; i++){
+        fprintf(file, "%d * %d = %d\n", num, i, num*i);
+    }
+
+    // Close the file
+    fclose(file);
+
     return 0;
 }
 
